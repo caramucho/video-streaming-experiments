2b765f8e4c9c9e4bb1768d7ad59a0a08593afb1d
Caramucho <zl.iafag@gmail.com>
2019-11-15 14:38:28 +0900

    add env

---
+++ env.py
@@ -0,0 +1,53 @@
+import numpy as np
+from load_trace import load_trace
+
+MILLISECONDS_IN_SECOND = 1000.0
+B_IN_MB = 1000000.0
+BITS_IN_BYTE = 8.0
+RANDOM_SEED = 42
+VIDEO_CHUNCK_LEN = 4000.0  # millisec, every time add this amount to buffer
+BITRATE_LEVELS = 6
+TOTAL_VIDEO_CHUNCK = 48
+BUFFER_THRESH = 60.0 * MILLISECONDS_IN_SECOND  # millisec, max buffer limit
+DRAIN_BUFFER_SLEEP_TIME = 500.0  # millisec
+PACKET_PAYLOAD_PORTION = 0.95
+LINK_RTT = 80  # millisec
+PACKET_SIZE = 1500  # bytes
+NOISE_LOW = 0.9
+NOISE_HIGH = 1.1
+VIDEO_SIZE_FILE = './video_size_'
+
+
+class Environment:
+    def __init__(self, all_cooked_time, all_cooked_bw, random_seed=RANDOM_SEED):
+        assert len(all_cooked_time) == len(all_cooked_bw)
+
+        np.random.seed(random_seed)
+
+        self.all_cooked_time = all_cooked_time
+        self.all_cooked_bw = all_cooked_bw
+
+        self.video_chunk_counter = 0
+        self.buffer_size = 0
+
+        # pick a random trace file
+        self.trace_idx = np.random.randint(len(self.all_cooked_time))
+        self.cooked_time = self.all_cooked_time[self.trace_idx]
+        self.cooked_bw = self.all_cooked_bw[self.trace_idx]
+
+        # randomize the start point of the trace
+        # note: trace file starts with time 0
+        self.mahimahi_ptr = np.random.randint(1, len(self.cooked_bw))
+        self.last_mahimahi_time = self.cooked_time[self.mahimahi_ptr - 1]
+
+        self.video_size = {}  # in bytes
+        for bitrate in range(BITRATE_LEVELS):
+            self.video_size[bitrate] = []
+            with open(VIDEO_SIZE_FILE + str(bitrate)) as f:
+                for line in f:
+                    self.video_size[bitrate].append(int(line.split()[0]))
+
+
+if __name__ == "__main__":
+    all_cooked_time, all_cooked_bw, _ = load_trace()
+    env = Environment(all_cooked_time, all_cooked_bw)

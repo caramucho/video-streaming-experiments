1d91289aa5ac6c4b7c88a4a72e2350e499b98c50
Caramucho <zl.iafag@gmail.com>
2019-11-18 13:59:31 +0900

    bb test done

--- bb.py
+++ bb.py
@@ -3,10 +3,11 @@ import fixed_env as env
 import load_trace
 
 
-S_INFO = 6  # bit_rate, buffer_size, next_chunk_size, bandwidth_measurement(throughput and time), chunk_til_video_end
+# bit_rate, buffer_size, next_chunk_size, bandwidth_measurement(throughput and time), chunk_til_video_end
+S_INFO = 6
 S_LEN = 8  # take how many frames in the past
 A_DIM = 6
-VIDEO_BIT_RATE = [300,750,1200,1850,2850,4300]  # Kbps
+VIDEO_BIT_RATE = [300, 750, 1200, 1850, 2850, 4300]  # Kbps
 M_IN_K = 1000.0
 REBUF_PENALTY = 4.3  # 1 sec rebuffering -> 3 Mbps
 SMOOTH_PENALTY = 1
@@ -32,7 +33,7 @@ def main():
                               all_cooked_bw=all_cooked_bw)
 
     log_path = LOG_FILE + '_' + all_file_names[net_env.trace_idx]
-    log_file = open(log_path, 'wb')
+    log_file = open(log_path, 'w')
 
     epoch = 0
     time_stamp = 0
@@ -48,8 +49,8 @@ def main():
         # the action is from the last decision
         # this is to make the framework similar to the real
         delay, sleep_time, buffer_size, rebuf, \
-        video_chunk_size, next_video_chunk_sizes, \
-        end_of_video, video_chunk_remain = \
+            video_chunk_size, next_video_chunk_sizes, \
+            end_of_video, video_chunk_remain = \
             net_env.get_video_chunk(bit_rate)
 
         time_stamp += delay  # in ms
@@ -57,9 +58,9 @@ def main():
 
         # reward is video quality - rebuffer penalty
         reward = VIDEO_BIT_RATE[bit_rate] / M_IN_K \
-                 - REBUF_PENALTY * rebuf \
-                 - SMOOTH_PENALTY * np.abs(VIDEO_BIT_RATE[bit_rate] -
-                                           VIDEO_BIT_RATE[last_bit_rate]) / M_IN_K
+            - REBUF_PENALTY * rebuf \
+            - SMOOTH_PENALTY * np.abs(VIDEO_BIT_RATE[bit_rate] -
+                                      VIDEO_BIT_RATE[last_bit_rate]) / M_IN_K
         r_batch.append(reward)
 
         last_bit_rate = bit_rate
@@ -91,14 +92,15 @@ def main():
             bit_rate = DEFAULT_QUALITY  # use the default action here
             r_batch = []
 
-            print "video count", video_count
+            print("video count", video_count)
             video_count += 1
 
             if video_count > len(all_file_names):
                 break
-
+            # stop test
+            break
             log_path = LOG_FILE + '_' + all_file_names[net_env.trace_idx]
-            log_file = open(log_path, 'wb')
+            log_file = open(log_path, 'w')
 
 
 if __name__ == '__main__':
